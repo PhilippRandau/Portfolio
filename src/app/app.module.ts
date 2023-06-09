2d5b980bee061b8d96ed9b01b96c949e972bd367
PhilippRandau <philipp-randau@web.de>
2023-06-09 17:19:21 +0200

    background and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderMenuComponent } from './header-menu/header-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderMenuComponent
   ],
   imports: [
     BrowserModule,
