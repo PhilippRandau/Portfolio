6af6012f683f739a2e30b8e72a0e022fb7e95e3c
PhilippRandau <philipp-randau@web.de>
2023-06-25 16:09:51 +0200

    responsive first

--- src/app/header-menu/header-menu.component.ts
+++ src/app/header-menu/header-menu.component.ts
@@ -9,11 +9,18 @@ export class HeaderMenuComponent {
   highlightAboutMe = false;
   highlightMySkills = false;
   highlightPortfolio = false;
+  highlightContact = false;
+  openMenu = false;
 
   public hideAllHighlights() {
     this.highlightAboutMe = false;
     this.highlightMySkills = false;
     this.highlightPortfolio = false;
+    this.highlightContact = false;
+  }
+
+  toggleMenu() {
+    this.openMenu = !this.openMenu;
   }
 }
 
