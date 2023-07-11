42d4bc6f1aafceba8e3ec2974d863068b2753e27
PhilippRandau <philipp-randau@web.de>
2023-07-11 12:59:31 +0200

    animation trigger adjustment

--- src/assets/services/shared.service.ts
+++ src/assets/services/shared.service.ts
@@ -2,11 +2,16 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class SharedService {
-  isScrolledIntoView(element, isElementScrolledIntoView){
+  isScrolledIntoView(element, isElementScrolledIntoView) {
     if (element) {
       const rect = element.nativeElement.getBoundingClientRect();
       const topShown = rect.top >= 0;
-      const bottomShown = rect.bottom <= window.innerHeight;
+      let bottomShown;
+      if (window.innerHeight - 110 <= rect.height) {
+        bottomShown = rect.bottom - rect.height <= window.innerHeight;
+      } else {
+        bottomShown = rect.bottom <= window.innerHeight;
+      }
       isElementScrolledIntoView = topShown && bottomShown;
       return isElementScrolledIntoView;
     }
